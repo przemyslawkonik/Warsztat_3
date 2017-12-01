76919b9ea68e72d4a195a2bf3633a914b129837d
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-01 15:16:09 +0100

    add view to browse members of particular group

---
+++ src/pl/coderslab/controller/GroupDetails.java
@@ -0,0 +1,52 @@
+package pl.coderslab.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.coderslab.dao.UserDao;
+
+/**
+ * Servlet implementation class GroupDetails
+ */
+@WebServlet("/groupDetails")
+public class GroupDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GroupDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			int id = Integer.parseInt(request.getParameter("id"));
+			request.setAttribute("users", UserDao.loadAllByGroupId(id));
+			getServletContext().getRequestDispatcher("/groupDetails.jsp").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}

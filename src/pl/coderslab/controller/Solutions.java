869bc0691bea27ddf624cca67e47186174565017
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-03 16:39:21 +0100

    add link to list all existing solutions

---
+++ src/pl/coderslab/controller/Solutions.java
@@ -0,0 +1,51 @@
+package pl.coderslab.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.coderslab.dao.SolutionDao;
+
+/**
+ * Servlet implementation class Solutions
+ */
+@WebServlet("/solutions")
+public class Solutions extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Solutions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			request.setAttribute("solutions", SolutionDao.loadAll());
+			getServletContext().getRequestDispatcher("/solutions.jsp").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
